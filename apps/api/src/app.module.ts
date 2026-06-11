7f8e997f9378b1bc6be41033d37cb0617a1e6b36
Santiago <santivillarley1010@gmail.com>
2026-06-11 13:30:28 -0600

    feat(api): wire feature modules into AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CatalogsModule } from './catalogs/catalogs.module';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
+import { PlanillaModule } from './planilla/planilla.module';
 
 @Module({
   imports: [
@@ -12,6 +16,10 @@ import { DatabaseModule } from './database/database.module';
       expandVariables: true,
     }),
     DatabaseModule,
+    AuthModule,
+    EmployeesModule,
+    PlanillaModule,
+    CatalogsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
